6e0f6fb85f4c61315943bb2115077fa355768ae7
Valentina <valentinamr@gmail.com>
2024-02-29 14:52:35 -0300

    Add media files for customer

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,10 +1,10 @@
-import ImageSlider from "./ImageSlider"
+import ImageSliderCustomers from "./ImageSliderCustomers"
 
 const Customer = () => {
   return (
     <div className="p-16">
       <h2 className="text-6xl text-center py-4">Customers & Teams</h2>
-      <ImageSlider/>
+      <ImageSliderCustomers/>
     </div>
   )
 }
---
+++ src/components/ImageSliderCustomers.jsx
@@ -0,0 +1,53 @@
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+const ImageSliderCustomers = () => {
+  const images = [
+    './assets/img/customers/customer1.jpg',
+    './assets/img/customers/customer2.jpg',
+    './assets/img/customers/customer3.jpg',
+    './assets/img/customers/customer4.jpg',
+    './assets/img/customers/customer6.jpg',
+    './assets/img/customers/customer7.jpg',
+    './assets/img/customers/customer8.jpg',
+    './assets/img/customers/customer9.jpg',
+    './assets/img/customers/customer10.jpg'
+  ];
+
+  return (
+    <Swiper
+    modules={[Navigation]}
+    slidesPerView={1}
+    spaceBetween={10}
+    navigation
+    breakpoints={{
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    }}
+    className="mySwiper"
+  >
+      {images.map((src, index) => (
+        <SwiperSlide key={index}>
+          <img src={src} alt={`Slide ${index + 1}`} width={400} className="rounded-lg" />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
+
+export default ImageSliderCustomers;
